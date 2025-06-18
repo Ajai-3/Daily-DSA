7060e26430757528f32d1899057526bb693758ab
Ajai-3 <ajaipjayan@gmail.com>
2025-06-18 10:46:43 +0530

    Fix filename comment and update character removal logic in commonChars function

--- String/commonChars.js
+++ String/commonChars.js
@@ -1,11 +1,11 @@
-// commonInString.js
+// commonChars.js
 function commonChars(str1, str2) {
     let result = []
     for (let i = 0; i < str1.length; i++) {
         for (let j = 0; j < str2.length; j++) {
             if (str1[i] === str2[j]) {
                 result.push(str1[i])
-                str2 = str2.slice(0, j) + str2.slice(j + 1)
+                str2[j] = ""
                 break
             }
         }
