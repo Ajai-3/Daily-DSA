64d3e6c6aa31a09143a8d76c0cdd9e7394e0a207
Ajai P Jayan <ajaipjayan@gmail.com>
2026-06-04 14:55:24 +0530

    Time: 9 ms (43.03%) | Memory: 68.9 MB (83.59%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -4,20 +4,16 @@
  */
 var productExceptSelf = function(nums) {
     let n = nums.length;
-    let result = [];
-    let suffix = new Array(n).fill(1)
-    let prefix = new Array(n).fill(1)
+    let result = new Array(n).fill(1);
 
     for (let i = 1; i < n; i++) {
-        prefix[i] = prefix[i - 1] * nums[i - 1]
+        result[i] = result[i - 1] * nums[i - 1]
     }
 
+    let suffix = 1;
     for (let i = n - 2; i >= 0; i--) {
-        suffix[i] = suffix[i + 1] * nums[i + 1]
-    }
-
-    for (let i = 0; i < n; i++) {
-        result.push(prefix[i] * suffix[i])
+        suffix *= nums[i + 1]
+        result[i] *= suffix
     }
 
     return result;
