7cb8b76796f65dc4330768f3b769c61db58aa604
Ajai P Jayan <ajaipjayan@gmail.com>
2026-06-04 15:36:29 +0530

    Time: 4 ms (18.49%) | Memory: 57.4 MB (25.36%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    let count = 0;
+
+    for (let i = 0; i < flowerbed.length; i++) {
+
+        if ((flowerbed[i - 1] === 0 || flowerbed[i - 1] === undefined ) && flowerbed[i] === 0 && (flowerbed[i + 1] === 0|| flowerbed[i + 1] === undefined )) {
+            count++
+            flowerbed[i] = 1
+        }
+
+
+    }
+   
+    return count >= n
+};
