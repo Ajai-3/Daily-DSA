b457fc75e3f0421edc61df84d5671d6cf805f031
Ajai P Jayan <ajaipjayan@gmail.com>
2026-06-11 18:35:09 +0530

    Time: 520 ms (5.26%) | Memory: 78.5 MB (32.83%) - LeetSync

---
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var goodNodes = function(root) {
+    let count = 0;
+
+    function traverse(node, prevVal) {
+        if (!node) return;
+        
+        let val = node.val;
+
+        if (val >= prevVal) {
+            count++
+            prevVal = val
+        }
+
+        console.log("Curr val", val, "Prev val", prevVal)
+
+        traverse(node.left, prevVal)
+        traverse(node.right, prevVal)
+        
+    }
+
+    traverse(root, -Infinity)
+
+    return count;
+};
